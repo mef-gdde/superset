e0096953ddca055576a7988a0ebd19c8912198ca
Boren Mon <borenmon.dev@gmail.com>
2026-03-19 15:15:34 +0700

    fix: update Docker image tag in build script to base-6.0.0

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker buildx build --platform linux/amd64,linux/arm64 -t registry-dev.e-invoice.gov.kh/data-ef/gdde-superset:pre-6.0.0 --load .
+docker buildx build --platform linux/amd64,linux/arm64 -t registry-dev.e-invoice.gov.kh/data-ef/gdde-superset:base-6.0.0 --load .
